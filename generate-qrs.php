a07458626330f83112079b499123601ceea0448b
Jesse TFB <langatjesse@gmail.com>
2025-09-09 16:39:07 +0300

    new changes

--- generate-qrs.php
+++ generate-qrs.php
@@ -146,8 +146,5 @@ foreach ($checkpoints as $checkpoint) {
             </div>
         <?php endif; ?>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" xintegrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </body>
 </html>
